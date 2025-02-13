fcb4e45ce3dfd1888a0c20419fb6ac3742e4a2f9
Vishal Kapgate <vishaldk26@gmail.com>
2025-02-14 01:43:03 +0530

    Strings

---
+++ 001-Basics/005-strings.js
@@ -0,0 +1,32 @@
+let car1 = "Mercedes";
+let car2 = 'Bugatti';
+console.log(typeof car1);
+console.log(typeof car2);
+
+// Strings can also be defined as objects
+let car3 = new String("Land Rover");
+console.log(typeof car3);
+
+let str1 = "That is a 'car'";
+let str2 = 'That is a "car"';
+
+// Template String
+let str = `These
+are cars. They are
+amazingly fast`;
+
+let myCar = "It\'s my car";
+
+const num = 5;
+let numCars = `I have ${5} cars`;
+
+console.log(car1);
+console.log(car2);
+console.log(car3);
+console.log(str1);
+console.log(str2);
+console.log(str);
+console.log(myCar);
+console.log(numCars)
+
+// Template Strings
