d9265caa512a054c1ec47048e6dacf852a0a424f
Vishal Kapgate <vishaldk26@gmail.com>
2025-02-13 14:31:31 +0530

    JS Basics

---
+++ 002-conditionals.js
@@ -0,0 +1,13 @@
+let weather = "rainy";
+
+/*
+    ==  --> Checks if values are equal or not
+    === --> Checks for value of equality and type of equality
+*/
+if(weather === "rainy"){
+    console.log("It's a rainy day");
+}else if(weather === "cloudy"){
+    console.log("It's a cloudy day");
+}else{
+    console.log("It's a sunny day");
+}
---
+++ 003-loops.js
@@ -0,0 +1,5 @@
+let cars = ["Mercedes", "BMW", "Audi", "Porsche"];
+
+for(let i = 0; i < cars.length; i++){
+    console.log(cars[i]);
+}
