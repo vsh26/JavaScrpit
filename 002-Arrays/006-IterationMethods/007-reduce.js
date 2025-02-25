17979d626264ab46c43874a4c11ad5c2880369f6
Vishal Kapgate <vishaldk26@gmail.com>
2025-02-25 17:10:25 +0530

    Added examples of reduce

--- 002-Arrays/006-IterationMethods/007-reduce.js
+++ 002-Arrays/006-IterationMethods/007-reduce.js
@@ -7,6 +7,10 @@
 
         arr.reduce(function(accumulator, current), initialValue)
 
+        accumulator - The value resulting from the previous call to callbackFn. On the first call, its value is initialValue if the latter is specified; otherwise its value is array[0].
+
+        current - The value of the current element. On the first call, its value is array[0] if initialValue is specified; otherwise its value is array[1]
+
     Exa:
         const initialValue = 0;
         const numbers = [1, 2, 3, 4, 5];
@@ -62,3 +66,38 @@ let count = fruits.reduce((accumulator, currentValue) => {
 }, {});
 
 console.log(count); // { apple: 3, banana: 2, orange: 1 }
+
+
+/*
+    Calculate total sales
+*/
+
+let SalesData = [
+    { product: "Laptop", price: 1200 },
+    { product: "Smartphone", price: 800 },
+    { product: "Headphones", price: 150 },
+    { product: "Keyboard", price: 80 }
+];
+
+const totalSales = SalesData.reduce((acc, sale) => acc + sale.price, 0);
+console.log(totalSales);
+
+
+
+/*
+
+    Find most active user
+
+*/
+
+let userActivity = [
+    { user: "Alice", activityCount: 45 },
+    { user: "Bob", activityCount: 72 },
+    { user: "Charlie", activityCount: 33 }
+];
+
+let mostActiveUser = userActivity.reduce((maxUser, user) =>
+    user.activityCount > maxUser.activityCount ? user : maxUser
+);
+
+console.log(mostActiveUser);
