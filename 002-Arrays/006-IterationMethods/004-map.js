681496dbb4e31aea849560803f7213b473d88c82
Vishal Kapgate <vishaldk26@gmail.com>
2025-02-26 13:09:36 +0530

    Map example

--- 002-Arrays/006-IterationMethods/004-map.js
+++ 002-Arrays/006-IterationMethods/004-map.js
@@ -14,3 +14,25 @@ const mapCars = cars.map(car =>{
 });
 
 console.log(mapCars);
+
+
+/*
+
+    Find average rating for each movie
+
+*/
+
+let movieRatings = [
+    { title: "Movie A", ratings: [4, 5, 3]},
+    { title: "Movie B", ratings: [5, 5, 4]},
+    { title: "Movie C", ratings: [3, 4, 2]}
+];
+
+let averageRatings = movieRatings.map((movie) => {
+    let sum = movie.ratings.reduce((acc, curr) => acc + curr, 0);
+    let average = sum/movie.ratings.length;
+
+    return {title: movie.title, averageRatings: average.toFixed(2)};
+});
+
+console.log(averageRatings);
