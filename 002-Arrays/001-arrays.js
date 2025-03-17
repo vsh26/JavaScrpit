5408f8ecd034863380c5c34a46120fb8cd2e3671
Vishal Kapgate <vishaldk26@gmail.com>
2025-03-18 02:06:26 +0530

    References and Destructuring

--- 002-Arrays/001-arrays.js
+++ 002-Arrays/001-arrays.js
@@ -23,3 +23,27 @@ console.log(typeof person);
 // Converting array to string
 const fruitString = fruits.toString();
 console.log(fruitString);
+
+// Arrays are references
+
+const arr1 = [1, 2, 3];
+const arr2 = arr1;
+
+arr2.push(4);
+console.log(arr1);  // [ 1, 2, 3, 4 ]
+console.log(arr2);  // [ 1, 2, 3, 4 ]
+
+const arr3 = [1, 2, 3];
+const arr4 = arr3.slice();
+
+arr4.push(4);
+console.log(arr3);  // [ 1, 2, 3]
+console.log(arr4);  // [ 1, 2, 3, 4 ]
+
+
+// Destructuring
+
+const [fisrtValue, secondValue] = [1, 2, 3, 4];
+
+console.log(fisrtValue);    // 1
+console.log(secondValue);   // 2
